f297e722b356f82596bd4ef1a7486e6b326c69b4
idn-fahmifn <fahmifn.idn@gmail.com>
2024-11-20 15:04:40 +0700

    fin

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// JIka admin, maka akan dimasukan ke route berikut : 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', AdminMiddleware::class])->name('dashboard');
+
+Route::middleware(['auth', AdminMiddleware::class])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// jika bukan admin maka akan diarahkan kerouting berikut : 
+Route::get('halaman-user', function(){
+    return view('dashboard-user');
+})->name('user');
+
+require __DIR__.'/auth.php';

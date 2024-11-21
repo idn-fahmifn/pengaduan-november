8248d65717a8fb8257b3b4bd41757e025cf21eb5
idn-fahmifn <fahmifn.idn@gmail.com>
2024-11-21 10:00:42 +0700

    table

---
+++ database/migrations/2024_11_21_025127_create_tanggapan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tanggapan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user'); //foreign Key ke tabel users.
+            $table->unsignedBigInteger('id_pengaduan'); //foreign Key ke table pengaduan.
+            $table->date('tgl_tanggapan');
+            $table->date('update_tanggapan');
+            $table->text('tanggapan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tanggapan');
+    }
+};
